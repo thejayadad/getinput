223fc240fe7aed3e00c1e1ccc2e75d35378922ae
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-11-05 09:31:21 -0400

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const greet = document.getElementById("greet");
+
+btn.addEventListener("click", () =>{
+    let name = input.value;
+    greet.innerText = `Good Morning ${name}`;
+    input.value = "";
+})
